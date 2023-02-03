9f52d921f4de12b9a191b10810d9472586607774
dane <danerismendoza096@gmail.com>
2023-02-03 19:17:52 +0800

    input_pullup

---
+++ INPUT_PULLUP/INPUT_PULLUP.ino
@@ -0,0 +1,23 @@
+int led = 2, button = 3;
+bool pressed = false;
+void setup() {
+pinMode(led, OUTPUT);
+pinMode(button, INPUT_PULLUP);
+
+
+Serial.begin(9600);
+Serial.println("success");
+}
+
+void loop() {
+  bool currentState = digitalRead(button);
+  if(currentState == pressed){
+    digitalWrite(led,HIGH);
+    Serial.println("Button is pressed");
+    delay(500);
+  }
+  else{
+    digitalWrite(led,LOW);
+    delay(500);
+  }
+}
